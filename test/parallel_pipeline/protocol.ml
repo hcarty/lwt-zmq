d2c13e9bab3f096cebfb83563192e4595c214991
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-02-26 00:07:16 -0500

    added some constants to protocol to keep configuration in 1 place

--- test/parallel_pipeline/protocol.ml
+++ test/parallel_pipeline/protocol.ml
@@ -1,3 +1,9 @@
+
+let random_str ~len = 
+  let rand_chr () = (Char.chr (97 + (Random.int 26))) in
+  let s = String.make len 'a' in
+  String.map (fun _ -> rand_chr ()) s
+
 module Address = struct
   let job = "tcp://127.0.0.1:6558"
   let result = "tcp://127.0.0.1:6557"
@@ -10,6 +16,14 @@ module Job = struct
   }
   let serialize (j : t) = Marshal.to_string j []
   let deserialize s = Marshal.from_string s 0
+
+  let jobs_per_second = 1
+
+  let (min_time,max_time) = (1, 2)
+
+  let random_job () =
+    let time = (Random.int max_time) + min_time in
+    { estimated_time=time; name=(random_str ~len:8) }
 end
 
 module Result = struct
@@ -20,4 +34,6 @@ module Result = struct
   }
   let serialize (j : t) = Marshal.to_string j []
   let deserialize s = Marshal.from_string s 0
+
+  let (min_time,max_time) = (2,4)
 end

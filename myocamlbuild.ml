80d852f13d937047f8ca2074c34fede20d8e2606
Hezekiah M. Carty <hez@0ok.org>
2013-05-02 16:18:36 -0400

    Prepare for 1.0-beta4
    Added send_all, recv_all, to_socket

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: a859cdb1b40cd0e5f4f25b6ff4a7a471) *)
+(* DO NOT EDIT (digest: fb8d1cc54544fc8a552c6197ec6812e8) *)
 module OASISGettext = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/oasis/OASISGettext.ml"
+(* # 21 "/home/hcarty/.opam/4.00.1/build/oasis.0.3.0/src/oasis/OASISGettext.ml" *)
 
   let ns_ str =
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/oasis/OASISExpr.ml"
+(* # 21 "/home/hcarty/.opam/4.00.1/build/oasis.0.3.0/src/oasis/OASISExpr.ml" *)
 
 
 
@@ -116,7 +116,7 @@ end
 
 # 117 "myocamlbuild.ml"
 module BaseEnvLight = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/base/BaseEnvLight.ml"
+(* # 21 "/home/hcarty/.opam/4.00.1/build/oasis.0.3.0/src/base/BaseEnvLight.ml" *)
 
   module MapString = Map.Make(String)
 
@@ -214,7 +214,7 @@ end
 
 # 215 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml"
+(* # 21 "/home/hcarty/.opam/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
 
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -323,7 +323,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+(* # 21 "/home/hcarty/.opam/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -339,7 +339,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
 
-# 56 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+(* # 56 "/home/hcarty/.opam/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   type t =
       {
@@ -480,7 +480,7 @@ let package_default =
      MyOCamlbuildBase.lib_ocaml = [("lwt-zmq", ["src"])];
      lib_c = [];
      flags = [];
-     includes = [("test", ["src"])];
+     includes = [("test/parallel_pipeline", ["src"]); ("test", ["src"])];
      }
   ;;
 

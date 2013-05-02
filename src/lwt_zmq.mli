be6124d557e262ea302a604205e5982be3847ea0
Hezekiah M. Carty <hez@0ok.org>
2013-05-02 15:37:04 -0400

    Add to_socket, send_all, recv_all; clean up some comments

--- src/lwt_zmq.mli
+++ src/lwt_zmq.mli
@@ -6,9 +6,22 @@ module Socket : sig
   (** [of_socket s] wraps the zeromq socket [s] for use with Lwt *)
   val of_socket : 'a ZMQ.Socket.t -> 'a t
 
-  (** [recv socket] waits for a message on [socket] without blocking other Lwt threads *)
+  (** [to_socket s] extracts the raw zeromq socket from [s] *)
+  val to_socket : 'a t -> 'a ZMQ.Socket.t
+
+  (** [recv socket] waits for a message on [socket] without blocking other Lwt
+      threads *)
   val recv : 'a t -> string Lwt.t
 
-  (** [send socket] sends a message on [socket] without blocking other Lwt threads *)
+  (** [send socket m] sends a message [m] on [socket] without blocking other
+      Lwt threads *)
   val send : 'a t -> string -> unit Lwt.t
+
+  (** [recv_all socket] waits for a multi-part message on [socket] without
+      blocking other Lwt threads *)
+  val recv_all : 'a t -> string list Lwt.t
+
+  (** [send_all socket m] sends all parts of the multi-part message [m] on
+      [socket] without blocking other Lwt threads *)
+  val send_all : 'a t -> string list -> unit Lwt.t
 end
